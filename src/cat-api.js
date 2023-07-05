4aa1526603246371d70cc0c2afbda14538248fbd
ewasawczynska <sawczynska.e@gmail.com>
2023-07-05 19:22:59 +0200

    functions / cat-api

---
+++ src/cat-api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import Notiflix from 'notiflix';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_7RIdaPQIKQeuQ4h5OHhIknuCKcTJHhxKZ6jGEa1GCIsFM4TeRDRzpC67rBRSSXOw';
+
+const checklist = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loaderInfo = document.querySelector('.loader');
+const errorInfo = document.querySelector('.error');
+const breeds = 'https://api.thecatapi.com/v1/breeds';
+const images = 'https://api.thecatapi.com/v1/images';
+
+function fetchBreeds() {
+  axios
+    .get(breeds)
+    .then(response => response.data)
+    .catch(error => console.error(error.name, error.message));
+}
+
+function fetchCatByBreed(breedId) {
+  axios
+    .get('${images}${breedId}')
+    .then(response => response.data)
+    .catch(error => console.error(error.name, error.message));
+}
